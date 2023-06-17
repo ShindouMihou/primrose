321251a3ddd47fa215f654090a8d5aac61cf720f
Shindou Mihou <git@mihou.pw>
2023-06-18 04:55:18 +0800

    feat(creator): do not retry for not founds

--- client/src/lib/requests/retries/retries.ts
+++ client/src/lib/requests/retries/retries.ts
@@ -1,10 +1,13 @@
-import {UNAUTHENTICATED} from "../errors/generic";
+import {NOT_FOUND, UNAUTHENTICATED} from "../errors/generic";
 import {token} from "../../stash";
 
 export const  AUTHENTICATED_RETRY = (failureCount, error) => {
     if (error === UNAUTHENTICATED) {
         token.set('')
         setTimeout(() => window.location.replace('/login'), 250)
+        return false
+    }
+    if (error === NOT_FOUND) {
         return false
     }
     return failureCount < 5;
