108cb8e90f599fe040db9a377c3d74f8cbdc0065
Shindou Mihou <git@mihou.pw>
2023-07-21 23:51:20 +0800

    feat: fix seo not working

--- client/src/routes/read/[key]/+page.server.ts
+++ client/src/routes/read/[key]/+page.server.ts
@@ -3,10 +3,10 @@ import type { PageServerLoad } from './$types';
 import isbot from 'isbot'
 
 export const load = (async ({ params, request }) => {
-    const useragent = request.headers.get('User-Aget')
+    const useragent = request.headers.get('User-Agent')
     if (isbot(useragent))  {
         const post = await fetchPost(params.key, null)
-        return  {
+        return {
             preload: { post }
         }
     }
